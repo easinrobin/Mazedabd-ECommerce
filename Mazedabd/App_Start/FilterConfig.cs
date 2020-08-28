295caef676e22fe56dedc01ca20b6cc90dc06559
Easin Robin <robinkhondokar@gmail.com>
2020-08-28 11:07:19 +0600

    Add project files.

---
+++ Mazedabd/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Mazedabd
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
